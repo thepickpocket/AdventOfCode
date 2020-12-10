964f9a2821520449978ea079325c3d3200034a16
Jason Richard Evans <evans.richard.jason@gmail.com>
2020-12-10 18:22:22 +0200

    Learning and improving original implementations
    From 45 Lines down to 24

--- src/main/kotlin/Day1/Day1.kt
+++ src/main/kotlin/Day1/Day1.kt
@@ -1,45 +1,24 @@
 package Day1
 
-fun solvePuzzle1(inputValues: List<String>): Int {
-    var i = 0;
-    var k = 0;
+/**
+ * 1. 'it' is used inside a lambda to refer to its parameter implicitly
+ * 2. 'hashset' is an optimization in search - O(1)
+ * 3. kotlin allows for 'nested'/'local' functions
+ */
 
-    while (i < inputValues.size) {
-        while (k < inputValues.size) {
-            if (inputValues[i].toInt() + inputValues[k].toInt() == 2020)
-                return inputValues[i].toInt() * inputValues[k].toInt()
-            else {
-                k++;
-            }
-        }
-        i ++;
-        k = 0;
-    }
+fun solvePuzzle1(inputValues: List<String>, sumToMatch: Int): Int {
+    val values = inputValues.map { it.toInt() }.toHashSet()
 
-    return -1;
+    return values.filter { values.contains(sumToMatch - it) }
+        .let { it[0] * it[1] }
 }
 
-fun solvePuzzle2(inputValues: List<String>): Int {
-    var i = 0;
-    var j = 0;
-    var k = 0;
+fun solvePuzzle2(inputValues: List<String>, sumToMatch: Int): Int {
+    val values = inputValues.map { it.toInt() }.toHashSet()
 
-    while (i < inputValues.size) {
-        while(j < inputValues.size) {
-            while (k < inputValues.size) {
-                if (inputValues[i].toInt() + inputValues[j].toInt() + inputValues[k].toInt() == 2020)
-                    return inputValues[i].toInt() * inputValues[j].toInt() * inputValues[k].toInt()
-                else {
-                    k++
-                }
-            }
-            j++
-            k = 0
-        }
-        i++;
-        j = 0;
-        k = 0;
-    }
+    fun filterSecondNumber(addUpTo: Int) = values.filter { values.contains(addUpTo - it) }
 
-    return -1;
+    return values
+        .filter { filterSecondNumber(sumToMatch - it).isNotEmpty() }
+        .let { it[0] * it[1] * it[2] }
 }
--- src/test/kotlin/Day1/Day1KtTest.kt
+++ src/test/kotlin/Day1/Day1KtTest.kt
@@ -9,8 +9,8 @@ class Day1KtTest {
     fun sampleInputTest() {
         val inputs = readFile("day1/sample1.txt");
 
-        val answer = solvePuzzle1(inputs)
-        val answer2 = solvePuzzle2(inputs)
+        val answer = solvePuzzle1(inputs, 2020)
+        val answer2 = solvePuzzle2(inputs, 2020)
 
         assertEquals(514579, answer)
 
@@ -21,8 +21,8 @@ class Day1KtTest {
     fun fullInputTest() {
         val inputs = readFile("day1/fullInput.txt");
 
-        val answer = solvePuzzle1(inputs)
-        val answer2 = solvePuzzle2(inputs)
+        val answer = solvePuzzle1(inputs, 2020)
+        val answer2 = solvePuzzle2(inputs, 2020)
 
         println("Day 1 Answer 1: $answer")
         assertEquals(444019, answer)
